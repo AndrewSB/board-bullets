1a3df1f31f52360b0d672878c83effe426e7c772
Andrew Breckenridge <asbreckenridge@me.com>
2015-01-05 01:47:34 -0800

    setup login screen

---
+++ Board-Bullets/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Board-Bullets
+//
+//  Created by Andrew Breckenridge on 1/5/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func logInHit(sender: AnyObject) {
+    }
+
+    @IBAction func signUpHit(sender: AnyObject) {
+    }
+
+    @IBAction func facebookButtonHit(sender: AnyObject) {
+    }
+
+    @IBAction func twitterButtonHit(sender: AnyObject) {
+    }
+
+}
