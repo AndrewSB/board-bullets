e66beb53999a3fe44e24aece35e460f288c54cd0
Andrew Breckenridge <asbreckenridge@me.com>
2015-01-16 16:35:36 -0800

    added a shitton of frameworks, down to one duplicate error

--- Board-Bullets/Board-Bullets-Bridging-Header.h
+++ Board-Bullets/Board-Bullets-Bridging-Header.h
@@ -3,4 +3,6 @@
 //
 
 #import <Parse/Parse.h>
+#import <FacebookSDK/FacebookSDK.h>
 #import <ParseFacebookUtils/PFFacebookUtils.h>
+#import "YZSwipeBetweenViewController/YZSwipeBetweenViewController.h"
