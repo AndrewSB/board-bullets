1c404a2cb35d5293abd0038d5627f150439099a1
Andrew Breckenridge <asbreckenridge@me.com>
2015-01-25 21:18:21 -0600

    finish submitQuestionVC

--- Board-Bullets/SubmitQuestionViewController.swift
+++ Board-Bullets/SubmitQuestionViewController.swift
@@ -15,11 +15,29 @@ class SubmitQuestionViewController: UIViewController {
     @IBOutlet weak var dummyAnswerLabel2: CircularEdgeTextField!
     @IBOutlet weak var categoryLabel: CircularEdgeTextField!
     @IBOutlet weak var subcategoryLabel: CircularEdgeTextField!
+    
+    let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
 
     override func viewDidLoad() {
+        addTextDismiss()
         super.viewDidLoad()
+        activityIndicator.center = CGPointMake(view.frame.size.width/2, view.frame.size.height/2)
 
     }
     @IBAction func submitButtonWasHit(sender: AnyObject) {
+        let question = PFObject(className: "UserQuestions")
+        question["category"] = categoryLabel.text.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        question["subcategory"] = subcategoryLabel.text.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        question["question"] = questionLabel.text.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        question["option1"] = dummyAnswerLabel.text.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        question["option2"] = dummyAnswerLabel2.text.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        question["option3"] = answerLabel.text.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        
+        view.addSubview(activityIndicator)
+        question.saveInBackgroundWithBlock({(PFBooleanResultBlock) in
+            println("saved")
+            self.activityIndicator.removeFromSuperview()
+        })
+    
     }
 }
