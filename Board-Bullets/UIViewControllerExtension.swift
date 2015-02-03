db32ab28c25a7e2314e149e704c3c6e5f43c5c18
Andrew Breckenridge <asbreckenridge@me.com>
2015-02-03 15:32:16 -0600

    add a little of the keyboard recognizer

--- Board-Bullets/UIViewControllerExtension.swift
+++ Board-Bullets/UIViewControllerExtension.swift
@@ -17,6 +17,41 @@ extension UIViewController {
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "hideKeyboard:"))
     }
     
+    func registerForKeyboard() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: <#String?#>, object: <#AnyObject?#>)
+    }
+    
+    func deregisterForKeyboard() {
+        
+    }
+    
+    - (void)registerForKeyboardNotifications {
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+    selector:@selector(keyboardWasShown:)
+    name:UIKeyboardDidShowNotification
+    object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+    selector:@selector(keyboardWillBeHidden:)
+    name:UIKeyboardWillHideNotification
+    object:nil];
+    
+    }
+    
+    - (void)deregisterFromKeyboardNotifications {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+    name:UIKeyboardDidHideNotification
+    object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+    name:UIKeyboardWillHideNotification
+    object:nil];
+    
+    }
+    
+    
     func hideKeyboard(sender: AnyObject) {
         println("lol")
         view.endEditing(true)
