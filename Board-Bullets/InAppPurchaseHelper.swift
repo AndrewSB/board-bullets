f07526ab95e720bad261be6528de303878cacc0c
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-05 11:16:16 -0700

    create In app purchase helper

---
+++ Board-Bullets/InAppPurchaseHelper.swift
@@ -0,0 +1,70 @@
+//
+//  InAppPurchaseHelper.swift
+//  Board-Bullets
+//
+//  Created by Andrew Breckenridge on 7/4/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+import Parse
+import Bolts
+
+
+// Look at all these observers. I LOVE Swift #nogettersorsetters
+class InAppPurchaseHelper: NSObject {
+    static let productIdentifier = "AllContentQuestions"
+    static let userStoreKey = "holmesboughttheapp" // lol
+    
+    class var bought: Bool {
+        get {
+            if let boughtBool = NSUserDefaults.standardUserDefaults().objectForKey(self.userStoreKey) as? Bool {
+                return boughtBool
+            }
+            return false
+        }
+        set {
+            NSUserDefaults.standardUserDefaults().setBool(newValue, forKey: self.userStoreKey)
+        }
+    }
+    
+    class var boughtFromParse: Bool {
+        get {
+        if PFUser.currentUser() != nil {
+            if let remoteBought = PFUser.currentUser()!["bought"] as? Bool {
+                return remoteBought
+            }
+        }
+        return false
+        }
+        set {
+        
+        }
+    }
+    
+    class var boughtProdut: SKProduct {
+        get {
+            let product = SKProduct()
+        }
+    }
+    
+    class var boughtProductRequest: SKRequest {
+        get {
+            return SKRequest()
+        }
+    }
+}
+
+extension InAppPurchaseHelper: SKRequestDelegate, SKRequestDelegate {
+    func requestDidFinish(request: SKRequest!) {
+        println("request did finish")
+    }
+    
+    func request(request: SKRequest!, didFailWithError error: NSError!) {
+        println("failed to request")
+    }
+    
+    
+}
