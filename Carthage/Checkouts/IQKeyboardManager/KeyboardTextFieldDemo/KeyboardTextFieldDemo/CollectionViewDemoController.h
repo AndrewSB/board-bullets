9268222c79a45643b01306b9c8451e7467ff63df
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-23 23:15:45 -0700

    check in dependcies because Carthage+namespaced Bolts is a b****

---
+++ Carthage/Checkouts/IQKeyboardManager/KeyboardTextFieldDemo/KeyboardTextFieldDemo/CollectionViewDemoController.h
@@ -0,0 +1,17 @@
+//
+//  CollectionViewDemoController.h
+//  IQKeyboard
+//
+//  Created by Iftekhar on 29/10/14.
+//  Copyright (c) 2014 Iftekhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#ifdef NSFoundationVersionNumber_iOS_5_1
+
+@interface CollectionViewDemoController : UIViewController
+
+@end
+
+#endif
