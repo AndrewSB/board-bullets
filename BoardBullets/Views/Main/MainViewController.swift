add1381d4de919ef473a1aa80b8a0e3f8cd4ffac
Andrew Breckenridge <asbreckenridge@me.com>
2015-10-11 16:20:30 -0700

    add switch for version to mainVC + configure ats

--- BoardBullets/Views/Main/MainViewController.swift
+++ BoardBullets/Views/Main/MainViewController.swift
@@ -12,13 +12,29 @@ import IQKeyboardManager
 class MainViewController: UIViewController {
     @IBOutlet weak var settingsButton: UIButton!
     
+    @IBOutlet weak var getFullVersionButton: UIButton!
+    
+    @IBOutlet weak var submitAQuestionButton: UIButton!
+    @IBOutlet weak var myPerformanceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         IQKeyboardManager.sharedManager().enable = true
         
+        switch InAppPurchase.bought {
+        case true:
+            getFullVersionButton.hidden = true
+        case false:
+            submitAQuestionButton.hidden = true
+            myPerformanceButton.hidden = true
+        }
+        
         addTextDismiss()
         view.endEditing(true)
+    }
+    
+    @IBAction func didHitGetFullVersion() {
     }
 
     @IBAction func unwindToMain(segue: UIStoryboardSegue) {}
